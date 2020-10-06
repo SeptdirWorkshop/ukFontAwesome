daa05e02bb41042df8c5db71ddf39db4e27b5dc2
Igor Berdichevkskiy <septdir@gmail.com>
2020-10-06 16:28:56 +0300

    Prepare 5.15.1 realise

--- dist/js/uikit-fa-regular-icons.js
+++ dist/js/uikit-fa-regular-icons.js
@@ -1,7 +1,7 @@
 /*
 * @package     ukFontAwesome
 * @subpackage  Regular icons
-* @version     5.15.0
+* @version     5.15.1
 * @author      Septdir Workshop - www.septdir.com
 * @copyright   Copyright (c) 2018 - 2020 Septdir Workshop. All rights reserved.
 * @license     MIT license: https://opensource.org/licenses/MIT
